03c53926cd052042aef287852da4fcac59985dcf
Vardaan-Grover <67010021+Vardaan-Grover@users.noreply.github.com>
2022-07-04 14:02:39 +0530

    Update helper_functions.py
    added a Dataset split function

--- helper_functions.py
+++ helper_functions.py
@@ -332,3 +332,27 @@ def get_lines(filename):
     '''
     with open(filename, 'r') as f:
         return f.readlines()
+      
+# Create a function to split a tf.data.Dataset instance into train, val and test splits
+def get_dataset_partitions(ds, train_split=0.8, val_split=0.1, test_split=0.1, shuffle=True, shuffle_size=10000):
+  '''
+  Takes in a tf.data.Dataset instance and splits it into different tf.data.Dataset instances for the purpose of train, val and test splits.
+  
+  Returns
+    3 Datasets in the order: `train_ds, val_ds, test_ds`
+  '''
+    ds_size = ds.cardinality().numpy()
+    assert (train_split + test_split + val_split) == 1
+    
+    if shuffle:
+        # Specify seed to always have the same split distribution between runs
+        ds = ds.shuffle(shuffle_size, seed=12)
+    
+    train_size = int(train_split * ds_size)
+    val_size = int(val_split * ds_size)
+    
+    train_ds = ds.take(train_size)    
+    val_ds = ds.skip(train_size).take(val_size)
+    test_ds = ds.skip(train_size).skip(val_size)
+    
+    return train_ds, val_ds, test_ds
