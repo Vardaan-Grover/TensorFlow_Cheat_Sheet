f0fa92e12723746a5c76ab712fe76f8be0da5e42
Vardaan-Grover <67010021+Vardaan-Grover@users.noreply.github.com>
2022-08-12 12:43:01 +0530

    added view_random_image function

--- helper_functions.py
+++ helper_functions.py
@@ -340,3 +340,19 @@ def create_model_checkpoint(model_name, save_path='model_experiments'):
                                               monitor='val_loss', # only output a minimum amount of text
                                               mode='min',
                                               save_best_only=True)
+  
+  # Create a function view random images from a directory
+  def view_random_image(target_dir, target_class):
+    # Setup target directory (we'll view images from here)
+    target_folder = target_dir+target_class
+
+    # Get a random image path
+    random_image = random.sample(os.listdir(target_folder), 1)
+
+    # Read in the image and plot it using matplotlib
+    img = mpimg.imread(target_folder + "/" + random_image[0])
+    plt.imshow(img)
+    plt.title(target_class)
+    plt.axis("off");
+
+    print(f"Image shape: {img.shape}") # show the shape of the image
