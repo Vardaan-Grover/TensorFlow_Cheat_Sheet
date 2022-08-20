6ba4a0c1ea7460979fc933db626ce7d23f8e6576
Vardaan-Grover <67010021+Vardaan-Grover@users.noreply.github.com>
2022-08-20 08:14:19 +0530

    added get_dataset_partitions functions

--- helper_functions.py
+++ helper_functions.py
@@ -356,3 +356,21 @@ def view_random_image(target_dir, target_class):
   plt.axis("off");
 
   print(f"Image shape: {img.shape}") # show the shape of the image
+  
+def get_dataset_partitions(ds, train_split=0.8, val_split=0.1, test_split=0.1, shuffle=True, shuffle_size=10000):
+    assert (train_split + test_split + val_split) == 1
+    ds_size = tf.data.experimental.cardinality(ds).numpy()
+
+    if shuffle:
+        # Specify seed to always have the same split distribution between runs
+        ds = ds.shuffle(shuffle_size, seed=12)
+
+
+    train_size = int(train_split * ds_size)
+    val_size = int(val_split * ds_size)
+    
+    train_ds = ds.take(train_size)    
+    val_ds = ds.skip(train_size).take(val_size)
+    test_ds = ds.skip(train_size).skip(val_size)
+    
+    return train_ds, val_ds, test_ds
